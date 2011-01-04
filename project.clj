cfb94d528f2220a6aec3b619c5962b7a2730d11e
Gregg Williams <gregg13@innerpaths.net>
2011-01-03 21:17:29 -0800

    Using 1.3 snapshot that makes "break" debugging work

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject ptest "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [overtone/clj-piccolo2d "0.0.3-SNAPSHOT"]]
   :dev-dependencies [[lein-run "1.0.1-SNAPSHOT"]
-		     [swank-clojure "1.2.1"]])
+		     [swank-clojure "1.3.0-SNAPSHOT"]])
